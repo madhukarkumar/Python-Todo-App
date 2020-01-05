6f8c0fdb3699f7843d9d8617dc454c289218de7b
Madhukar <madhukar.kumar@gmail.com>
2020-01-05 14:03:09 -0800

    First checkin. Has working version of AJAX with Try catch for session commit.

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import sys
 
 from flask import Flask, abort, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/todo/create', methods=['POST'])
